dd9bfd29cf3a731a387b2fbd98236adffe1cf6ac
mayowa <matthewoluwatotin2020@gmail.com>
2024-09-10 22:48:40 +1245

    initla

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
+
+
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 //middlewares
 app.use(express.json())
 app.use(cors())
